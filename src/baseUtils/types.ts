ad5979a7bfff58eb7ce0d35e0fdce4ecc4e179f4
Prysya <bloodofabyss@yandex.ru>
2021-12-24 09:44:20 +0300

    feat: add new interfaces for json request object

--- src/baseUtils/types.ts
+++ src/baseUtils/types.ts
@@ -1,8 +1,8 @@
-import { AxiosResponse } from "axios";
+import { AxiosResponse } from 'axios';
 
 /**
  * Types
-*/
+ */
 export type TAppId = string;
 export type TUrl = string;
 export type TData = string;
@@ -12,21 +12,36 @@ export type TItems = Element[] | [];
 export type TAxiosPromiseResponse = Promise<AxiosResponse>;
 export type TSerialize = string;
 export type TErrorMessage = string;
+export type TJsonData = IJsonReqest[] | undefined;
+export type TEmptyObject = Record<string, never>;
 
 /**
-* Interfaces
-*/
+ * Interfaces
+ */
+export interface IJsonReqest {
+  ascii: string;
+  event_ms: number;
+  event_time: string;
+  value: string;
+}
+
+export interface IParsedJson {
+  [key: string]: Omit<IJsonReqest, 'ascii'>;
+}
+
 export interface INotSerializedData {
-  [key:string]: number | string; 
+  [key: string]: number | string;
 }
 
 export interface IBaseUtilsInterface {
   requestForXml(url: TUrl, data: TData): Promise<TItems>;
+  requestForJSON(url: TUrl, data: TData): Promise<IParsedJson | TEmptyObject>;
   _baseRequest(url: TUrl, data: TData): TAxiosPromiseResponse;
   _parseToXml(xmlText: TXmlText): TXmlParsedData;
   _serialize(data: INotSerializedData): TSerialize;
   _parseErrorMessage(xmlText: TXmlText): string;
   _getItems(xmlParsed: TXmlParsedData): TItems;
+  _parseJsonData(jsonData: TJsonData): IParsedJson | TEmptyObject;
   setDebug(): void;
   _checkDebug(): boolean;
 }
