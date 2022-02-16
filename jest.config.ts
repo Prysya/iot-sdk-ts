0e2fca49073630994a9f89a5e3696f99ef8c5d49
Prysya <bloodofabyss@yandex.ru>
2022-02-16 16:17:53 +0300

    feat: add preset and moduleDirectories to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,11 @@
-import type {Config} from '@jest/types';
+import type { Config } from '@jest/types';
 
 export default async (): Promise<Config.InitialOptions> => {
   return {
     verbose: true,
     testEnvironment: 'jsdom',
-    testURL: 'https://test.com'
+    preset: 'ts-jest',
+    testURL: 'https://test.com',
+    moduleDirectories: ['node_modules', 'src']
   };
 };
